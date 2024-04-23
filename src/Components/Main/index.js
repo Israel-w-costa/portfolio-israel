02f9c1a6b5a8fad71ac151df80a609856c8d5a49
israel <israelwendellc@gmail.com>
2024-04-23 20:21:55 -0300

    responsive the mobile

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -5,20 +5,26 @@ export default function Main() {
     return (
         <div className="main">
             <div className="main_text">
-                <h1>
-                    Programador React
+                <div className="main_text-h1">
+                    <h1>
+                        Programador React
 
-                </h1>
-                <p>
-                    Sou estudante de programação há um ano. Durante esse período aprendi
-                    ferramentas como HTMl, CSS, e aprofundei na linguagem JavaScript.
-                    Atualmente meu objetivo é ter o domínio no Framework React interagindo com a
-                    linguagem JS. Ainda mais busco minha primeira oportunidade no mercado de TI.
-                </p>
-                <a 
-                href="https://wa.me/5591981028327?text=opa%2C+visitei+seu+site+do+seu+portfolio%2C+tudo+bem%3F"
-                className="main_text-a">
-                    Entrar em contato </a>
+                    </h1>
+                </div>
+                <div className="main_text-container">
+                    <p>
+                        Sou estudante de programação há um ano. Durante esse período aprendi
+                        ferramentas como HTMl, CSS, e aprofundei na linguagem JavaScript.
+                        Atualmente meu objetivo é ter o domínio no Framework React interagindo com a
+                        linguagem JS. Ainda mais busco minha primeira oportunidade no mercado de TI.
+                    </p>
+                </div>
+                <div className="main_text-container-a">
+                    <a
+                        href="https://wa.me/5591981028327?text=opa%2C+visitei+seu+site+do+seu+portfolio%2C+tudo+bem%3F"
+                        className="main_text-a">
+                        Entrar em contato </a>
+                </div>
             </div>
 
             <div className="main_sectionTwo">
@@ -27,9 +33,9 @@ export default function Main() {
                     alt="Imagem de perfil do Israel "
                 ></img>
                 <div className="main_link">
-                    <a  
-                    href="https://wa.me/5591981028327?text=opa%2C+visitei+seu+site+do+seu+portfolio%2C+tudo+bem%3F"
-                    className="main_link-a"
+                    <a
+                        href="https://wa.me/5591981028327?text=opa%2C+visitei+seu+site+do+seu+portfolio%2C+tudo+bem%3F"
+                        className="main_link-a"
                     >Entrar em contato </a>
                 </div>
             </div>
